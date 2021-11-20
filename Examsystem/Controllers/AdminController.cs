add82091d6b3a0221ab70bc3a387ed652848ad2b
admin <admin@DESKTOP-4GOMJFS>
2021-11-20 10:09:15 +0530

    Add project files.

---
+++ Examsystem/Controllers/AdminController.cs
@@ -0,0 +1,311 @@
+﻿using Examsystem.Models;
+using LinqToExcel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity.Validation;
+using System.Data.OleDb;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Examsystem.Controllers
+{
+    public class AdminController : Controller
+    {
+        OnlineExaminationEntities db = new OnlineExaminationEntities();
+     
+       
+        public ActionResult AdminLogin()
+        {
+            return View();
+        }
+
+        public ActionResult Logoutadmin()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AdminLogin(admin admin)
+        {
+            var checklogin = db.admins.Where(x => x.admin_id.Equals(admin.admin_id) && x.admin_password.Equals(admin.admin_password)).FirstOrDefault();
+            if (checklogin != null)
+            {
+                Session["Admin_id"] = admin.admin_id.ToString();
+                Session["Admin_password"] = admin.admin_password.ToString();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Notification = "wrong username or password";
+            }
+            return RedirectToAction("Dashboard");
+        }
+        
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(Category category)
+        {
+            if (db.Categories.Any(x => x.category_id == category.category_id))
+            {
+                ViewBag.Notification = "This subject is already existed";
+                return View();
+            }
+            else if (db.Categories.Any(x => x.category_name == category.category_name))
+            {
+                ViewBag.Notification = "This subject is already existed";
+                return View();
+            }
+            else
+            {
+
+                db.Categories.Add(category);
+                db.SaveChanges();
+                return View();
+            }
+        }
+
+
+        public ActionResult Getdetailsstudents()
+        {
+            List<Candidate> candidates = db.Candidates.ToList();
+            ViewBag.TotalStudents = candidates.Count();
+            return View(candidates);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public FileResult DownloadExcel()
+        {
+            string path = "/excelfolder/Questions.xlsx";
+            return File(path, "application/vnd.ms-excel", "Questionss.xlsx");
+        }
+
+        [HttpPost]
+        public JsonResult UploadExcel(Question question, HttpPostedFileBase FileUpload)
+        {
+
+            List<string> data = new List<string>();
+            if (FileUpload != null)
+            {
+
+                if (FileUpload.ContentType == "application/vnd.ms-excel" || FileUpload.ContentType == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+                {
+                    string filename = FileUpload.FileName;
+                    string targetpath = Server.MapPath("~/excelfolder/");
+                    FileUpload.SaveAs(targetpath + filename);
+                    string pathToExcelFile = targetpath + filename;
+                    var connectionString = "";
+                    if (filename.EndsWith(".xls"))
+                    {
+                        connectionString = string.Format("Provider=Microsoft.Jet.OLEDB.4.0; data source={0}; Extended Properties=Excel 8.0;", pathToExcelFile);
+                    }
+                    else if (filename.EndsWith(".xlsx"))
+                    {
+                        connectionString = string.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=\"Excel 12.0 Xml;HDR=YES;IMEX=1\";", pathToExcelFile);
+                    }
+
+                    var adapter = new OleDbDataAdapter("SELECT * FROM [Sheet1$]", connectionString);
+                    var ds = new DataSet();
+                    adapter.Fill(ds, "ExcelTable");
+                    DataTable dtable = ds.Tables["ExcelTable"];
+                    string sheetName = "Sheet1";
+                    var excelFile = new ExcelQueryFactory(pathToExcelFile);
+                    var artistAlbums = from a in excelFile.Worksheet<Question>(sheetName) select a;
+                    foreach (var a in artistAlbums)
+                    {
+                        try
+                        {
+                            if (a.Q_text != "" && a.QA != "")
+                            {
+                                Question TU = new Question();
+                                TU.Qid = a.Qid;
+                                TU.Q_text = a.Q_text;
+                                TU.QA = a.QA;
+                                TU.QB = a.QB;
+                                TU.QC = a.QC;
+                                TU.QD = a.QD;
+                                TU.Qcorrectans = a.Qcorrectans;
+                                TU.category_id = a.category_id;
+                                TU.levelid = a.levelid;
+                                db.Questions.Add(TU);
+                                db.SaveChanges();
+                            }
+                            else
+                            {
+                                data.Add("<ul>");
+                                if (a.Q_text == "" || a.Q_text == null) data.Add("<li> Text is required</li>");
+                                if (a.QA == "" || a.QA == null) data.Add("<li> option 1 is required</li>");
+                                if (a.QB == "" || a.QB == null) data.Add("<li> option 2 is required</li>");
+                                if (a.QC == "" || a.QC == null) data.Add("<li> option 1 is required</li>");
+                                if (a.QD == "" || a.QD == null) data.Add("<li> option 1 is required</li>");
+                                if (a.Qcorrectans == "" || a.Qcorrectans == null) data.Add("<li>Correct answer is required</li>");
+                                if (a.category_id == null) data.Add("<li> option 1 is required</li>");
+                                if (a.levelid == null) data.Add("<li> option 1 is required</li>");
+                                data.Add("</ul>");
+                                data.ToArray();
+                                return Json(data, JsonRequestBehavior.AllowGet);
+                            }
+                        }
+                        catch (DbEntityValidationException ex)
+                        {
+                            foreach (var entityValidationErrors in ex.EntityValidationErrors)
+                            {
+                                foreach (var validationError in entityValidationErrors.ValidationErrors)
+                                {
+                                    Response.Write("Property: " + validationError.PropertyName + " Error: " + validationError.ErrorMessage);
+                                }
+                            }
+                        }
+                    }
+
+                    if ((System.IO.File.Exists(pathToExcelFile)))
+                    {
+                        System.IO.File.Delete(pathToExcelFile);
+                    }
+                    return Json("success", JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+
+                    data.Add("<ul>");
+                    data.Add("<li>Only Excel file format is allowed</li>");
+                    data.Add("</ul>");
+                    data.ToArray();
+                    return Json(data, JsonRequestBehavior.AllowGet);
+                }
+            }
+            else
+            {
+                data.Add("<ul>");
+                if (FileUpload == null) data.Add("<li>Please choose Excel file</li>");
+                data.Add("</ul>");
+                data.ToArray();
+                return Json(data, JsonRequestBehavior.AllowGet);
+            }
+        }
+        public ActionResult Getdetailsques()
+        {
+            List<Question> questions = db.Questions.ToList();
+            return View(questions);
+        }
+        public ActionResult Delete(int id)
+        {
+            using (var context = new OnlineExaminationEntities())
+            {
+                var data = context.Questions.FirstOrDefault(x => x.Qid == id);
+                if (data != null)
+                {
+                    context.Questions.Remove(data);
+                    context.SaveChanges();
+                    return RedirectToAction("Getdetailscat");
+                }
+                else
+                    return View();
+            }
+        }
+        public ActionResult Addexam()
+        {
+            var catlist = db.Categories.ToList();
+            ViewBag.Catlist = new SelectList(catlist, "category_id", "category_name");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Addexam(exam exam)
+        {
+            int i = 3;
+            if (db.exams.Any(x => x.exam_id == exam.exam_id))
+            {
+                ViewBag.Notification1 = "This exam id is already existed";
+                return View();
+            }
+            else if (db.exams.Any(x => x.levelid > i))
+            {
+                ViewBag.Notification2 = "This level should be within 3";
+                return View();
+            }
+            else
+            {
+                var catlist = db.Categories.ToList();
+                ViewBag.Catlist = new SelectList(catlist, "category_id", "category_name");
+                db.exams.Add(exam);
+                db.SaveChanges();
+                return View();
+            }
+        }
+        public ActionResult GetdetailsExams()
+        {
+            List<exam> exams = db.exams.ToList();
+            return View(exams);
+        }
+        public ActionResult Edit(string id)
+        {
+            var cat = db.exams.Where(s => s.exam_id == id).SingleOrDefault();
+            return View(cat);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(exam exam,string id)
+        {
+            var cat = db.exams.FirstOrDefault(s => s.exam_id ==id);
+            if (cat != null)
+            {
+                cat.exam_description = exam.exam_description;
+                cat.category_id = exam.category_id;
+                cat.levelid = exam.levelid;
+                cat.duration = exam.duration;
+                cat.Noofquestion = exam.Noofquestion;
+                cat.marks = exam.marks;
+                cat.passmarks = exam.passmarks;
+                cat.totalmarks = exam.totalmarks;
+                db.SaveChanges();
+                return RedirectToAction("GetdetailsExams");
+            }
+            else
+                return View();
+
+            
+            
+        }
+        public ActionResult Deleteexam(string id)
+        {
+            using (var context = new OnlineExaminationEntities())
+            {
+                var data = context.exams.FirstOrDefault(x => x.exam_id == id);
+                if (data != null)
+                {
+                    context.exams.Remove(data);
+                    context.SaveChanges();
+                    return RedirectToAction("GetdetailsExams");
+                }
+                else
+                    return View();
+            }
+        }
+        public ActionResult Results(string id)
+        {
+            var d = (from e in db.Reports where string.Equals(e.user_id, id) select e).SingleOrDefault();
+            var data = db.Reports.FirstOrDefault(x => string.Equals(x.user_id, id));
+            if (data != null)
+            {
+                ViewBag.reportid = d.result_id;
+                ViewBag.score = d.result_score;
+                ViewBag.status = d.result_status;
+                ViewBag.uid = d.user_id;
+                ViewBag.eid = d.exam_id;
+                return View(d);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Examsystem/Controllers/UserController.cs
@@ -0,0 +1,275 @@
+﻿using Examsystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Examsystem.Controllers
+{
+    public class UserController : Controller
+    {
+        OnlineExaminationEntities db = new OnlineExaminationEntities();
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(Candidate candidate)
+        {
+            if (db.Candidates.Any(x => x.Userid == candidate.Userid))
+            {
+                ViewBag.Notification = "this account is already existed";
+                return View();
+            }
+            else
+            {
+                db.Candidates.Add(candidate);
+                db.SaveChanges();
+                Session["Username"] = candidate.Userid.ToString();
+                Session["User_fname"] = candidate.Userfname.ToString();
+                Session["User_Lname"] = candidate.Userlname.ToString();
+                Session["User_phoneeno"] = candidate.phoneno.ToString();
+                Session["User_email"] = candidate.email.ToString();
+                Session["user_password"] = candidate.password.ToString();
+                Session["confirm_password"] = candidate.confirmpassword.ToString();
+                return RedirectToAction("showexam");
+            }
+
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Candidate candidate)
+        {
+            var checklogin = db.Candidates.Where(x => x.Userid.Equals(candidate.Userid) && x.password.Equals(candidate.password)).FirstOrDefault();
+            if (checklogin != null)
+            {
+                Session["Username"] = candidate.Userid.ToString();
+                TempData["uid"] = candidate.Userid;
+                Session["user_password"] = candidate.password.ToString();
+                return RedirectToAction("showexam");
+            }
+            else
+            {
+                ViewBag.Notification = "wrong username or password";
+            }
+            return RedirectToAction("showexam");
+        }
+        public ActionResult showexam()
+        {
+            List<Category> categories = db.Categories.ToList();
+            return View(categories);
+        }
+        public ActionResult StartExam(string id)
+        {
+
+            using (var context = new OnlineExaminationEntities())
+            {
+                var d = (from e in db.exams where e.category_id == id && e.levelid == 1 select e).SingleOrDefault();
+                var data = context.exams.FirstOrDefault(x => x.category_id == id && x.levelid == 1);
+                List<Question> li = db.Questions.Where(x => x.category_id == id && x.levelid == 1).ToList();
+                Queue<Question> queue = new Queue<Question>();
+                foreach (Question a in li)
+                {
+                    queue.Enqueue(a);
+                }
+                TempData["questions "] = queue;
+                TempData["score"] = 0;
+                TempData["i"] = 1;
+                TempData["id"] = id;
+                TempData["pscore"] = d.passmarks;
+                TempData.Keep();
+                if (data != null)
+                {
+
+                    ViewBag.examid = d.exam_id;
+                    ViewBag.examtext = d.exam_description;
+                    ViewBag.categoryid = d.category_id;
+                    ViewBag.lid = d.levelid;
+                    ViewBag.duration = d.duration;
+                    ViewBag.noofque = d.Noofquestion;
+                    ViewBag.pm = d.passmarks;
+                    ViewBag.marks = d.marks;
+                    ViewBag.tmarks = d.totalmarks;
+                    TempData["level"] = d.levelid;
+                    TempData["examid"] = d.exam_id;
+                    TempData.Keep();
+                    return View(d);
+                }
+                else
+                    return View();
+            }
+        }
+        [HttpGet]
+        public ActionResult examques(string id)
+        {
+            Question q = null;
+            if (TempData["questions "] != null)
+            {
+                Queue<Question> qlist = (Queue<Question>)TempData["questions "];
+                if (qlist.Count > 0)
+                {
+                    q = qlist.Peek();
+                    qlist.Dequeue();
+                    TempData["questions "] = qlist;
+                    TempData.Keep();
+                }
+                else
+                {
+                    return RedirectToAction("Endexam");
+                }
+
+            }
+            return View(q);
+
+        }
+        [HttpPost]
+        public ActionResult examques(Question question)
+        {
+            string correctans = null;
+            if (question.QA != null)
+            {
+                correctans = "A";
+            }
+            else if (question.QB != null)
+            {
+                correctans = "B";
+            }
+            else if (question.QC != null)
+            {
+                correctans = "C";
+
+            }
+            else if (question.QD != null)
+            {
+                correctans = "D";
+            }
+            if (correctans.Equals(question.Qcorrectans))
+            {
+                TempData["score"] = Convert.ToInt32(TempData["score"]) + 1;
+            }
+
+            TempData.Keep();
+            return RedirectToAction("examques");
+        }
+        public ActionResult Endexam()
+        {
+            var level = Convert.ToInt32(TempData["level"]);
+            if (level == 3)
+            {
+                return RedirectToAction("End");
+            }
+            return View();
+        }
+        public ActionResult level2(int id)
+        {
+            var p = Convert.ToInt32(TempData["pscore"]);
+            if (id >= p)
+            {
+                return RedirectToAction("StartExam1");
+            }
+            else
+            {
+                return RedirectToAction("Home", "Index");
+            }
+           
+        }
+        public ActionResult StartExam1()
+        {
+
+            var c = TempData["id"].ToString();
+            ViewBag.exam =c;
+            var l = Convert.ToInt32(TempData["i"]) + 1;
+            using (var context = new OnlineExaminationEntities())
+            {
+                var d = (from e in db.exams where string.Equals(e.category_id,c) && e.levelid == l select e).SingleOrDefault();
+                var data = context.exams.FirstOrDefault(x => string.Equals(x.category_id,c) && x.levelid == l);
+                List<Question> li = db.Questions.Where(x => string.Equals(x.category_id,c) && x.levelid == l).ToList();
+                Queue<Question> queue = new Queue<Question>();
+                foreach (Question a in li)
+                {
+                    queue.Enqueue(a);
+                }
+                TempData["questions "] = queue;
+                TempData["score"] = 0;
+                TempData.Keep();
+                if (data != null)
+                {
+                    ViewBag.examid = data.exam_id;
+                    ViewBag.examtext = d.exam_description;
+                    ViewBag.categoryid = d.category_id;
+                    ViewBag.lid = d.levelid;
+                    ViewBag.duration = d.duration;
+                    ViewBag.noofque = d.Noofquestion;
+                    ViewBag.pm = d.passmarks;
+                    ViewBag.marks = d.marks;
+                    ViewBag.tmarks = d.totalmarks;
+                    TempData["level"] = d.levelid;
+                    TempData["pscore"] = d.passmarks;
+                    TempData["examid"] = d.exam_id;
+                    TempData["i"] = 2;
+                    TempData.Keep();
+                    return View(d);
+                }
+                else
+                    return View();
+            }
+        }
+        public ActionResult End()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Results ()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Results(Report report)
+        {
+            OnlineExaminationEntities db1 = new OnlineExaminationEntities();
+            var c = Convert.ToInt32(TempData["score"]);
+            var u = Convert.ToInt32(TempData["uid"]).ToString();
+            var e = Convert.ToInt32(TempData["examid"]).ToString();
+            report.result_score = c;
+            report.user_id = u;
+            report.exam_id = e;
+            var level = Convert.ToInt32(TempData["level"]);
+            if (level == 3)
+            {
+                report.result_status = "excellent";
+            }
+            else if (level == 2)
+            {
+                report.result_status = "good";
+            }
+            else if (level == 1)
+            {
+                report.result_status = "better try";
+            }
+            if (ModelState.IsValid)
+            {
+                db1.Reports.Add(report);
+                db1.SaveChanges();
+                return View();
+            }
+            return View();
+        }
+
+    }
+}
